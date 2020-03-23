5aa81000e16902838eb949737ca1fbeb2778e233
darkfantasy88 <59861596+darkfantasy88@users.noreply.github.com>
2020-03-23 15:44:39 -0500

    Auhm New Stuff IGuess

--- Prison_database/dbo/Tables/PrisonTbl.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[PrisonTbl]
-(
-	[PrisonID] INT NOT NULL constraint PK_PRISONTBL PRIMARY KEY,
-	[PrisonName] nvarchar(max) not null,
-	[CurrentCapacity] int  not null, 
-	[MaxCapacity] int not null, 
-	[TypeofPrison] nvarchar(200) not null
-
-)
